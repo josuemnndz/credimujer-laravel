1db127ceef7a7f89a4ddf54ac4c324a440b29f14
josuemnndz <31413766+josuemnndz@users.noreply.github.com>
2018-04-09 18:13:30 -0600

    update

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,81 +11,11 @@ use Carbon\Carbon;
 class PageController extends Controller
 {
     public function home(){       
-        $page = Page::buscar('home'); //busca el slug del admin
-  
-        	return view('home')->with('page',$page); 
-
-    }
-
-    public function nuestraInstitucion(){
-	    	$page = Page::buscar('nuestra-institucion', false);
-	    	return view('pages.nuestra_institucion')->with([ 'page'=> $page ]);
-    }
-
-
-    public function modeloEducativo(){
-	    	$page = Page::buscar('modelo-educativo',false);
-	    	return view('pages.modelo_educativo')->with([ 'page'=> $page ]);
-    }
-
-    public function noticias(){
-	    	$bloque = new Bloque; //instancia de bloque
-	    	$lastPosts = Post::where('category_id', '2')->orderBy('id','DESC')->limit(3)->get();//get primeros 3 post
-	    	$firstPost = Post::where('category_id', '2')->orderByRaw("RAND()")->limit(1)->get();//get ultimo post
-	    	$otherPosts = Post::where('category_id', '2')->orderByRaw("RAND()")->limit(3)->get();//get post aleatorios 
-
-	    return view('pages.noticias')->with('bloque',$bloque)->with('lastPosts',$lastPosts)->with('firstPost',$firstPost)->with('otherPosts',$otherPosts);
-    }
-
-    public function noticia($slug){
-	     	Carbon::setLocale('es'); //localizar El Salvador
-	     	$bloque = new Bloque; //instancia de bloque
-	        $post = Post::where('slug', $slug)->where('category_id', '2')->first();//post #
-
-	        if( $post  ){
-	        	$lastPosts = Post::where('category_id', '2')->orderBy('id','DESC')->limit(3)->get();//get primeros 3 post
-	        	return view('pages.noticia_interna')->with('post',$post)->with('bloque',$bloque)->with('lastPosts',$lastPosts);
-	        }
-
-        return view('errors.404');
-
-    }
-
-    public function alumnosExitosos(){
-    	$page = Page::buscar('alumno_exitoso',false);
-    	$bloque = new Bloque();
-    	$post = Post::where('category_id', '1')->first();//post #
-    	
-
-    	if( $post ){
-        	$lastAlumnos = Post::where('category_id', '2')->orderByRaw('id','DESC')->limit(3)->get();//get primeros 3 post
-        	$lastPostNoticias = Post::where('category_id','2')->orderByRaw("RAND()")->limit(3)->get();
-            return view('pages.alumnos_exitosos')->with('post',$post)->with('bloque',$bloque)->with('lastAlumnos',$lastAlumnos)->with('pagina','alumnos_exitosos')->with('lastPostNoticias', $lastPostNoticias);
-        }
-
-    	return view('errors.404');
-    }
-
-    public function alumnoExitoso($slug){
-    	$page = Page::buscar('alumno_exitoso',false);
-    	$bloque = new Bloque();
-    	$post = Post::where('slug', $slug)->where('category_id', '1')->first();//post #
-    	
-
-    	if( $post ){
-        	$lastAlumnos = Post::where('category_id', '1')->orderByRaw("RAND()")->limit(3)->get();//get primeros 3 post
-        	return view('pages.alumnos_exitosos')->with('post',$post)->with('bloque',$bloque)->with('lastAlumnos',$lastAlumnos);
-        }
-
-    	return view('errors.404');
-    }
-
-    public function contactanos(){
-    	$page = Page::buscar('contactanos',false);
-
-    	return view('pages.contactanos')->with([ 'page'=> $page ]);
-    }
-
-
-    
+        $page = Page::buscar('home'); //busca el slug del admin  
+        	return view('home')->with('page',$page);
+    }  
+    // public function nuestraInstitucion(){
+	   //  	$page = Page::buscar('nuestra-institucion', false);
+	   //  	return view('pages.nuestra_institucion')->with([ 'page'=> $page ]);
+    // }    
 }
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -3,10 +3,12 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="theme-color" content="#FFDB17" />
+    <meta name="theme-color" content="#c649a2" />
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title> @yield('title') </title>
+    <title> @yield('title') Credicomer Sociedad de Ahorro y Dinero</title>
+
+    <link rel="shortcut icon" href="{{{ asset('image/favicon.png') }}}">
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
